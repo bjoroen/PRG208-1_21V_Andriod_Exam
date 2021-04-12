35f914392a95e7697e65f300df86eb82c715590a
bjoroen <31513139+bjoroen@users.noreply.github.com>
2021-04-12 20:46:44 +0200

    Initial commit

---
+++ app/src/main/java/no/kristiania/prg208_1_21v_andriod_exam/MainActivity.kt
@@ -0,0 +1,11 @@
+package no.kristiania.prg208_1_21v_andriod_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
